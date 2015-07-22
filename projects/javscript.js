505d70e39a548579a2caa1e44eb7558458ad87b4
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2015-07-22 01:01:21 +0000

    Added new website

--- projects/javscript.js
+++
@@ -1,4 +0,0 @@
-    $('img').ready(function() {
-        $('div#fade-in').css('background-image','url(https://lh3.googleusercontent.com/iFAwaRxTD-Ou_Q8m8KcVRqeezWcRjM57mTzKBjZ-u9B1=w4000-h2000)');
-        $('div#fade-in').fadeIn(1500);
-    });
