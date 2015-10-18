72be67098bc03d6467830f2793641347483de6f9
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2015-10-18 19:05:07 -0400

    Added Themes

--- javscript.js
+++ javscript.js
@@ -1,5 +1,8 @@
     $('img').ready(function () {
-        $('body').css('background-image','url(https://lh3.googleusercontent.com/SsUEqWF4cua2t0vU-_Te2HuSzW5zuupN0rOoQPvMngYX=w2400-h1200)');
+
         $('body').hide().fadeIn(1500);
     });
 //div#fade-in
+/*
+        $('body').css('background-image','url(https://lh3.googleusercontent.com/SsUEqWF4cua2t0vU-_Te2HuSzW5zuupN0rOoQPvMngYX=w2400-h1200)');
+        */
