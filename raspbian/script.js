28337fd4e19917865cfb28cbaea97e4f5056b69e
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2018-08-26 12:03:32 -0400

    Remove console.log statements and replace timing in setIntervals

--- raspbian/script.js
+++ raspbian/script.js
@@ -18,8 +18,8 @@ $(document).ready(function() {
                 update();
         });
         
-        setInterval(getSunRiseSet, 4000); //6 hours 21600000
-        setInterval(update, 3000); //10 minutes 600000
+        setInterval(getSunRiseSet, 21600000); //6 hours
+        setInterval(update, 600000); //10 minutes
 
         function getSunRiseSet(callback) {
                 $.get("https://api.sunrise-sunset.org/json?lat=41.15588648&lng=-80.0806389&formatted=0", function(data) {
@@ -27,7 +27,6 @@ $(document).ready(function() {
                         sunset = data.results.sunset;
                         sunriseMilli = Date.parse(sunrise); // - 14400000 to get current timezone
                         sunsetMilli = Date.parse(sunset);
-                        console.log("The current time is " + new Date().getTime());
                         if (callback) {
                                 callback();     
                         }
@@ -42,14 +41,12 @@ $(document).ready(function() {
                         if (window.location.href == "file:///C:/Users/frees/Documents/GitHub/ianspryn.github.io/raspbian/raspbian-dark.html" || window.location.href == "http://ian.spryn.me/raspbian/raspbian-dark.html") {
                                 window.location.href = ("index.html");
                         }
-                        console.log("IT IS DAYTIME");
 
                 } else {
                         //Otherwise, it is night time, so go to dark page
                         if (window.location.href == "file:///C:/Users/frees/Documents/GitHub/ianspryn.github.io/raspbian/index.html" || window.location.href == "http://ian.spryn.me/raspbian/index.html") {
                                 window.location.href = ("raspbian-dark.html");
                         }
-                        console.log("IT IS NIGHTTIME");
                 }
         };
 });
