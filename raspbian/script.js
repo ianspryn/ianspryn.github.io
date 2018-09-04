a5226b3c6db2c5e646c7066822753496d0ce8f1a
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2018-09-04 11:44:48 -0400

    Update for new main website URL change

--- raspbian/script.js
+++ raspbian/script.js
@@ -7,27 +7,18 @@ $(document).ready(function() {
         var sunriseMilli;
         var sunsetMilli;
 
-
-        
-        
-
-        
-
         //INITIAL RUN
         getSunRiseSet(function() {
                 update();
         });
         
-        setInterval(getSunRiseSet, 4000); //6 hours 21600000
-        setInterval(update, 3000); //10 minutes 600000
+        setInterval(getSunRiseSet, 21600000); //6 hours
+        setInterval(update, 1200000); //20 minutes
 
         function getSunRiseSet(callback) {
                 $.get("https://api.sunrise-sunset.org/json?lat=41.15588648&lng=-80.0806389&formatted=0", function(data) {
-                        sunrise = data.results.sunrise;
-                        sunset = data.results.sunset;
-                        sunriseMilli = Date.parse(sunrise); // - 14400000 to get current timezone
-                        sunsetMilli = Date.parse(sunset);
-                        console.log("The current time is " + new Date().getTime());
+                        sunriseMilli = Date.parse(data.results.sunrise);
+                        sunsetMilli = Date.parse(data.results.sunset);
                         if (callback) {
                                 callback();     
                         }
@@ -39,17 +30,15 @@ $(document).ready(function() {
                 
                 if ((new Date().getTime()) > sunriseMilli && (new Date().getTime()) << sunsetMilli) {
                         //If it is after sunrise and before sunset, go to light page
-                        if (window.location.href == "file:///C:/Users/frees/Documents/GitHub/ianspryn.github.io/raspbian/raspbian-dark.html" || window.location.href == "http://ian.spryn.me/raspbian/raspbian-dark.html") {
+                        if (window.location.href == "file:///C:/Users/frees/Documents/GitHub/ianspryn.github.io/raspbian/raspbian-dark.html" || window.location.href == "https://ianspryn.com/raspbian/raspbian-dark.html") {
                                 window.location.href = ("index.html");
                         }
-                        console.log("IT IS DAYTIME");
 
                 } else {
                         //Otherwise, it is night time, so go to dark page
-                        if (window.location.href == "file:///C:/Users/frees/Documents/GitHub/ianspryn.github.io/raspbian/index.html" || window.location.href == "http://ian.spryn.me/raspbian/index.html") {
+                        if (window.location.href == "file:///C:/Users/frees/Documents/GitHub/ianspryn.github.io/raspbian/index.html" || window.location.href == "https://ianspryn.com/raspbian/index.html") {
                                 window.location.href = ("raspbian-dark.html");
                         }
-                        console.log("IT IS NIGHTTIME");
                 }
         };
 });
