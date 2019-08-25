c99cd08e84c3aac4fe8702099ae06deee221c99d
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2019-08-25 16:33:57 -0400

    apparently I forgot to merge?

--- raspbian/script.js
+++ raspbian/script.js
@@ -13,23 +13,12 @@ $(document).ready(function() {
         });
         
         setInterval(getSunRiseSet, 21600000); //6 hours
-<<<<<<< HEAD
-        setInterval(update, 600000); //10 minutes
-
-        function getSunRiseSet(callback) {
-                $.get("https://api.sunrise-sunset.org/json?lat=41.15588648&lng=-80.0806389&formatted=0", function(data) {
-                        sunrise = data.results.sunrise;
-                        sunset = data.results.sunset;
-                        sunriseMilli = Date.parse(sunrise); // - 14400000 to get current timezone
-                        sunsetMilli = Date.parse(sunset);
-=======
         setInterval(update, 1200000); //20 minutes
 
         function getSunRiseSet(callback) {
                 $.get("https://api.sunrise-sunset.org/json?lat=41.15588648&lng=-80.0806389&formatted=0", function(data) {
                         sunriseMilli = Date.parse(data.results.sunrise);
                         sunsetMilli = Date.parse(data.results.sunset);
->>>>>>> 169dee8d904a1244a1dba16dcd7aa7dfcf99e6b2
                         if (callback) {
                                 callback();     
                         }
